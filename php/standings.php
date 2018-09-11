88e91412ede111062883de3ae123109fee4b8ab5
JJ Burke <jburke18@alumni.nd.edu>
2018-09-11 13:05:59 +1000

    Fixed standings table

--- php/standings.php
+++ php/standings.php
@@ -11,19 +11,35 @@ if (!$conn) {
 }
 $sql = "
 select
-standTeam as Team,
+case standTeam when 'jj burke' then 'JJ Burke'
+when 'chris hammitt' then 'Chris Hammitt'
+when 'tom buckley' then 'Tom Buckley'
+when 'billy beirne' then 'Billy Beirne'
+when 'mike derusso' then 'Mike DeRusso'
+when 'mark krizmanich' then 'Mark Krizmanich'
+when 'chris curtin' then 'Chris Curtin'
+when 'matthew singer' then 'Matthew singer'
+when 'jordan hiller' then 'Jordan Hiller'
+when 'joe young' then 'Joe Young'
+when 'mike guiltinan' then 'Mike Guiltinan'
+when 'parker king' then 'Parker King'
+when 'ricky garcia' then 'Ricky Garcia'
+when 'andrew lamb' then 'Andrew Lamb'
+else
+	standTeam end as Team,
 wins as Wins,
 losses as Losses,
 tie as Tie,
 pointsScored as 'Points Scored',
 round(pointsScored/(wins+losses+tie),2) as 'Average Points',
-round(exPointAverage/(13-(wins+losses+tie)),2) as 'Expected Remaining Point Average',
-round(exWins,2) as 'Expected Remaining Wins',
+round(exPointAverage/(13-(wins+losses+tie)),2) as 'Expected End of Year Point Average',
+round(exWins,2) as 'Expected End of Year Wins',
 round(playoffsOdds,2) as 'Playoffs Odds',
 round(lowpoints,2) as 'Lowest Points Odds',
 round(highpoints,2) as 'Highest Points Odds',
 round(champOdds,2) as 'Champ Odds'
 from analysis.standings
+where standWeek = (select max(standWeek) from analysis.standings)
 order by playoffsOdds desc,
 champOdds desc
 ";
