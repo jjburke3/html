fe2ec86f63ddc6f4273afe490ed87743d2612d83
James Burke <jburke18@alumni.nd.edu>
2019-10-24 14:40:18 +1100

    Added a page for head to head matchups

---
+++ php/matchups.php
@@ -0,0 +1,44 @@
+<?php
+include('credentials.php');
+
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+$sql = "
+select winTeam, winOpp, (sum(winWin) + 0.5*sum(winTie))/count(*) as winPct from la_liga_data.wins
+where playoffs is not null or winWeek <= 13
+group by 1,2
+";
+
+
+$result = mysqli_query($conn,$sql);
+$data = array();
+
+
+
+for ($x = 0; $x < mysqli_num_rows($result); $x++) {
+	$data[] = mysqli_fetch_assoc($result);
+}
+
+echo json_encode($data);
+
+/*
+if(mysqli_num_rows($result) > 0) {
+		// output data of each row
+		while($row = mysqli_fetch_assoc($result)) {
+			echo "PROGRAM: " . $row["PROGRAM"]. " - QUOTES: " . $row["QUOTES"]. "<br>";
+		}
+	} else {
+	echo "0 results";
+}
+
+
+*/
+
+mysqli_close($conn);
+?>
